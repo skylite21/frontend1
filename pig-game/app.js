a1361c435f79ab06486242ddd7f202665da864b0
zsolt.lengyel21@gmail.com <Lengyel Zsolt>
2022-11-12 14:35:47 +0100

    pgsql is more strict, increase the hackiness up to 11

--- pig-game/app.js
+++ pig-game/app.js
@@ -20,34 +20,52 @@
 // változó deklarálás
 let scores, roundScore, activePlayer;
 
-// a játékosok pontszámai, mindkét játákos null ponttal indul
-// értékadás : value assignment
-scores = [0, 0];
 
-// forduló alatt megszerzett pontok
-roundScore = 0;
+function newGame() {
 
-// az első játékos kezd
-activePlayer = 0;
+  // a játékosok pontszámai, mindkét játákos null ponttal indul
+  // értékadás : value assignment
+  scores = [0, 0];
 
-// dom manipuláció (dom: document object model = HTML kód)
+  // forduló alatt megszerzett pontok
+  roundScore = 0;
 
-// kiválsztjuk a score-0 id-vel rendelkező html elemet
-// és a tartalmát beállítjuk 0-ra
-document.querySelector('#score-0').textContent = 0;
+  // az első játékos kezd
+  activePlayer = 0;
 
-// camelCase: mindenÚjSzótNagyBetűvelÍrunk // JavaScript
-// PascalCase: AzElsőBetűIsNagybetű // JavaScript
-// snake_case: minden_szó_között_alsóvonás_van // Python
-// kebab-case: a-szavak-között-kötőjel-van  // CSS
+  // dom manipuláció (dom: document object model = HTML kód)
+
+  // kiválsztjuk a score-0 id-vel rendelkező html elemet
+  // és a tartalmát beállítjuk 0-ra
+  document.querySelector('#score-0').textContent = 0;
+
+  // camelCase: mindenÚjSzótNagyBetűvelÍrunk // JavaScript
+  // PascalCase: AzElsőBetűIsNagybetű // JavaScript
+  // snake_case: minden_szó_között_alsóvonás_van // Python
+  // kebab-case: a-szavak-között-kötőjel-van  // CSS
 
 
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+  document.querySelector('#score-1').textContent = 0;
+  document.querySelector('#current-0').textContent = 0;
+  document.querySelector('#current-1').textContent = 0;
 
-// a játék indításakor a kocka még nem látszik:
-document.querySelector('.dice').style.display = 'none';
+  // a játék indításakor a kocka még nem látszik:
+  document.querySelector('.dice').style.display = 'none';
+  document.querySelector('.btn-hold').style.display = 'block';
+  document.querySelector('.btn-roll').style.display = 'block';
+
+  document.querySelector('#name-0').textContent = 'Player 1';
+  document.querySelector('#name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+
+
+  document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');
+}
+
+newGame();
 
 // a kokca dobás, gombra kattintás
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -117,6 +135,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     nextPlayer();
   }
 
-
-
 });
+
+
+
+document.querySelector('.btn-new').addEventListener('click', newGame);
